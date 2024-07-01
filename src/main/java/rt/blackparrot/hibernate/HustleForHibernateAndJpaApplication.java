45129eaced3d77ecf0ef2cfd2532ffc0a5f512ed
white-parrot <everydaycodestory@gmail.com>
2024-07-01 10:59:33 +0530

    [001]: Initial Project Structure

---
+++ src/main/java/rt/blackparrot/hibernate/HustleForHibernateAndJpaApplication.java
@@ -0,0 +1,13 @@
+package rt.blackparrot.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustleForHibernateAndJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HustleForHibernateAndJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rt/blackparrot/hibernate/HustleForHibernateAndJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package rt.blackparrot.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustleForHibernateAndJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
